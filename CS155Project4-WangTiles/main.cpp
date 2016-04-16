36454c20bbb97cc5e45cb7077381f1f75100c7c2
王小天 <wxt1994@gmail.com>
2016-04-15 19:32:43 -0700

    first commit

---
+++ CS155Project4-WangTiles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS155Project4-WangTiles
+//
+//  Created by 王小天 on 16/4/15.
+//  Copyright (c) 2016年 Xiaotian Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
